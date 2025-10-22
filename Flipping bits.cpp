053a6d1e7b6867b9dc00180bf08102d45980eb42
rizve_kazi <168621934+Rizve-Kazi@users.noreply.github.com>
2025-10-23 02:16:07 +0600

    Create Flipping bits.cpp

---
+++ Flipping bits.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        unsigned int n;cin>>n;
+        unsigned int ans=0;
+
+        cout<<(~n)<<endl;
+    }
+}
